77b241957242f06556427fc9eed2b57c30c592eb
djmckenzie77 <djmckenzie@madisonacademy.com>
2019-01-04 10:20:02 -0600

    final js edit

--- app.js
+++ app.js
@@ -2,10 +2,38 @@ const celsiusInput = document.querySelector('#celsius > input');
 const fahrenheitInput = document.querySelector('#fahrenheit > input');
 const kelvinInput = document.querySelector('#kelvin > input');
 
-celsiusInput.addEventListener('input', function() {
+function roundNum(num) {
+    return Math.round(num*100)/100;
+}
+
+function celsiusToFahrenheitAndKelvin() {
     const cTemp = parseFloat(celsiusInput.value);
     const fTemp = (cTemp * (9/5)) + 32;
     const kTemp = cTemp + 273.15;
-    fahrenheitInput.value = fTemp;
-    kelvinInput.value = kTemp;
-})
+    fahrenheitInput.value = roundNum(fTemp);
+    kelvinInput.value = roundNum(kTemp);
+}
+
+function FahrenheitToKelvinAndCelsius() {
+    const fTemp = parseFloat(fahrenheitInput.value);
+    const cTemp = (fTemp - 32) * (5/9);
+    const kTemp = (fTemp - 459.67) * (5/9);
+    celsiusInput.value = roundNum(cTemp);
+    kelvinInput.value = roundNum(kTemp);
+}
+
+function KelvinToCelsiusAndFahrenheit() {
+    const kTemp = parseFloat(kelvinInput.value);
+    const fTemp = 9/5 * (kTemp - 273) + 32;
+    const cTemp = kTemp - 273.15;
+    fahrenheitInput.value = roundNum(fTemp);
+    celsiusInput.value = roundNum(cTemp);
+}
+
+function main() {
+    celsiusInput.addEventListener('input', celsiusToFahrenheitAndKelvin);
+    fahrenheitInput.addEventListener('input', FahrenheitToKelvinAndCelsius);
+    kelvinInput.addEventListener('input', KelvinToCelsiusAndFahrenheit);
+}
+
+main();
